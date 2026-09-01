90a4ed4e30b0e6b39a5bf7747b1145d735b3dd8a
CorbanAllan <34634236+CorbanAllan@users.noreply.github.com>
2026-09-01 16:04:53 +0100

    added basic comments feature

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -1,20 +1,25 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.PostRepository;
+import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.List;
 
 @Controller
 public class PostsController {
 
     @Autowired
     PostRepository repository;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/posts")
     public String index(Model model) {
@@ -26,7 +31,22 @@ public class PostsController {
 
     @PostMapping("/posts")
     public RedirectView create(@ModelAttribute Post post) {
+        DefaultOidcUser principal = (DefaultOidcUser) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        User currentUser = userRepository
+                .findByOktaUserId(principal.getSubject())
+                .orElseThrow(() ->
+                        new IllegalStateException("User not found in local database")
+                );
+
+        Long databaseUserId = currentUser.getId();
+
+        post.setUserId(databaseUserId);
         repository.save(post);
+
         return new RedirectView("/posts");
     }
 }
---
+++ src/main/resources/db/migration/V10__alter_userid_data_type_to_bigint_comments.sql
@@ -0,0 +1,2 @@
+ALTER TABLE comments
+ALTER COLUMN user_id TYPE BIGINT;
