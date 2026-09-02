0989ab1b6bf3742412a8a5f04672f9fb73296dcb
Jennifer McDowell <218159393+JenMcDowell@users.noreply.github.com>
2026-09-02 14:09:41 +0100

    add likes

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -1,7 +1,9 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Like;
 import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.repository.LikeRepository;
 import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +27,9 @@ public class PostsController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    LikeRepository likeRepository;
 
     @GetMapping("/posts")
     public String index(Model model) {
@@ -73,6 +78,34 @@ public class PostsController {
         repository.save(post);
 
         return new RedirectView("/posts");
+    }
+    @PostMapping("/posts/{postId}/like")
+    public RedirectView likes (@PathVariable Long postId) {
+        likeRepository.findByPostId(postId);
+
+        DefaultOidcUser principal = (DefaultOidcUser)
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getPrincipal();
+
+        User currentUser = userRepository
+                .findByOktaUserId(principal.getSubject())
+                .orElseThrow(() ->
+                        new IllegalStateException(
+                                "User not found in local database"
+                        )
+                );
+
+        likeRepository
+                .findByPostIdAndUserId(postId, currentUser.getId())
+                .ifPresentOrElse(
+                        existingLike ->
+                                likeRepository.delete(existingLike),
+                        () -> likeRepository.save(new Like(postId, currentUser.getId()))
+                );
+                return new RedirectView("/posts");
+
     }
 }
 
