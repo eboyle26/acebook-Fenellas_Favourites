6a174336fd1a47b59dc17e793130fa6083362c51
Katie Brown <katiejjbrown@outlook.com>
2026-09-03 10:11:12 +0100

    First selenium tests

--- src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
+++ src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
@@ -9,6 +9,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class SignUpTest {
 
     WebDriver driver;
@@ -30,12 +37,27 @@ public class SignUpTest {
     public void successfulSignUpAlsoLogsInUser() {
         String email = faker.name().username() + "@email.com";
 
-        driver.get("http://localhost:8081/");
+        driver.get("http://localhost:8080/");
         driver.findElement(By.linkText("Sign up")).click();
         driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("password")).sendKeys("P@55qw0rd");
+        driver.findElement(By.name("password")).sendKeys("P@55qw0rd123456789");
         driver.findElement(By.name("action")).click();
         String greetingText = driver.findElement(By.id("greeting")).getText();
-        assertEquals("Signed in as " + email, greetingText);
+        assertEquals("Signed in as\n" + email, greetingText);
+    }
+
+    @Test
+    public void checkSignedInUserViewsFriendPage() {
+        String email = faker.name().username() + "@email.com";
+
+        driver.get("http://localhost:8080/");
+        driver.findElement(By.linkText("Sign up")).click();
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("P@55qw0rd123456789");
+        driver.findElement(By.name("action")).click();
+
+        driver.findElement(By.id("friends_button")).click();
+        String friendSubheaderText = driver.findElement(By.id("friends_h2")).getText();
+        assertEquals("Your friends", friendSubheaderText);
     }
 }
